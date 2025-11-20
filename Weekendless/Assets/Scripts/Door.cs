6767bd5b5c3d3949f1f3000437081313156fbc68
Ilya Z <grommma5@gmail.com>
2025-11-20 13:40:16 +0300

    V2.6
    Добавлен скрипт открытия двери в ванную; исправлено застревание героя в дверном проёме путём изменения его размеров; исправлена проблема с отсутствующей текстовой подсказкой у входной двери квартиры.

--- Weekendless/Assets/Scripts/Door.cs
+++ Weekendless/Assets/Scripts/Door.cs
@@ -3,25 +3,35 @@ using UnityEngine;
 public class Door : MonoBehaviour, IInteractableDoor
 {
     [Header("Door Settings")]
+    public Transform doorPivot;     
     public float openAngle = 90f; 
     public float openSpeed = 2f;  
 
     private bool isOpen = false;
     private Quaternion closedRotation;
     private Quaternion openRotation;
+
     public bool IsOpen => isOpen;
     public string DoorName => "Wooden Door";
 
     void Start()
     {
-        closedRotation = transform.rotation;
-        openRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, openAngle, 0));
+        if (doorPivot == null)
+            doorPivot = transform; 
+
+        closedRotation = doorPivot.rotation;
+        openRotation = Quaternion.Euler(doorPivot.eulerAngles + new Vector3(0, openAngle, 0));
     }
 
     void Update()
     {
         Quaternion targetRotation = isOpen ? openRotation : closedRotation;
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * openSpeed);
+
+        doorPivot.rotation = Quaternion.Slerp(
+            doorPivot.rotation, 
+            targetRotation, 
+            Time.deltaTime * openSpeed
+        );
     }
 
     public void ToggleDoor()
